948bb0a6a5056674ce1073d9a0846bdf722c3dab
dharlabwustl <atulkumar@wustl.edu>
2025-06-16 18:31:58 -0500

    Initial Commit

--- STEPS_COMPARTMENTS_CSF.sh
+++ STEPS_COMPARTMENTS_CSF.sh
@@ -2,6 +2,11 @@ echo " I AM WORKING"
 SESSION_ID=${1}
 ## STEPS 1: Registration of template ct (COLIHM62) to a session CT.
 /software/lin_transform_before_deepreg_COLIHM62_as_moving.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
+rm -r /workinginput/*
+rm -r /workingoutput/*
+rm -r /input1/*
+rm -r /output/*
+rm -r /outputinsidedocker/*
 /software/lin_transform_before_deepreg_COLIHM62_as_moving_mat_on_mask.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
 #/software/deepregbasedregis_csf_cistern_midline_separation_with_COLIHM62_NO_RAPIDS.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
 #/software/compartment_separation_with_vent_afterdeepreg_with_cistern_with_COLIHM62_with_midline.sh  $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST
