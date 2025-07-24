a96fb731cd4d5ccc0b8ac92f3136daaf046e7db7
dharlabwustl <atulkumar@wustl.edu>
2025-07-24 15:59:55 -0500

    Initial Commit

--- STEPS_COMPARTMENTS_CSF.sh
+++ STEPS_COMPARTMENTS_CSF.sh
@@ -19,7 +19,7 @@ clean_directories
 /software/lin_transform_before_deepreg_COLIHM62_as_moving_mat_on_mask.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
 clean_directories
 /software/deepregbasedregis_csf_cistern_midline_separation_with_COLIHM62_NO_RAPIDS.sh $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST /input1 /output
-clean_directories
-/software/compartment_separation_with_vent_afterdeepreg_with_cistern_with_COLIHM62_with_midline.sh  $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST
-clean_directories
-/software/csf_compartments_vols_N_display_for_NON_SAH.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
+#clean_directories
+#/software/compartment_separation_with_vent_afterdeepreg_with_cistern_with_COLIHM62_with_midline.sh  $SESSION_ID $XNAT_USER $XNAT_PASS $XNAT_HOST
+#clean_directories
+#/software/csf_compartments_vols_N_display_for_NON_SAH.sh ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
